011a501262b5e2fde8e4956d8df0846ab75eda84
Sosnovskikh Kirill <sosnovskix.kir2001@gmail.com>
2024-04-14 22:23:35 +0300

    Update db.py

--- bot/db.py
+++ bot/db.py
@@ -6,15 +6,3 @@ from config import mongodb_settings
 client = AsyncIOMotorClient(mongodb_settings.url)
 db = client.get_database(mongodb_settings.NAME)
 payments_collection = db.get_collection(mongodb_settings.COLLECTION)
-
-
-async def make_session() -> AsyncIOMotorClientSession:
-    global client
-
-    _session = await client.start_session()
-    try:
-        yield _session
-    except Exception:
-        pass
-    finally:
-        await _session.end_session()
